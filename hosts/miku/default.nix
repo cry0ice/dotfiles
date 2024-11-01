3fdf85981022946a798a7a6b865c6cdd538507f6
Cryo <cryo@disroot.org>
2024-11-02 00:10:08 +0800

    Update initrd

--- hosts/miku/default.nix
+++ hosts/miku/default.nix
@@ -17,16 +17,9 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.plymouth.enable = true;
-  boot.consoleLogLevel = 0;
-  boot.initrd.verbose = false;
   boot.kernelParams = [
     "quiet"
     "splash"
-    "boot.shell_on_fail"
-    "loglevel=3"
-    "rd.systemd.show_status=false"
-    "rd.udev.log_level=3"
-    "udev.log_priority=3"
     "psi=1"
   ];
 
